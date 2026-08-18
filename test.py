6f00f8215402b693acd639d53bdd22e8e0d90628
Denis Pashkov <thetskerp@MacBook-Air--Denis.local>
2026-08-18 13:44:27 +0300

    add and get category(ies)

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-from database.db import init_db, add_user, user_exists
+from database.db import init_db, add_user, add_category, get_categories
 
-db_name = "finance_bot.db"
+DB_NAME = "finance_bot.db"
 tg_1 = 123456789
 tg_2 = 222222222
 
-init_db(db_name)
-add_user(db_name, 123456789)
+init_db(DB_NAME)
 
-print(user_exists(db_name, tg_1))
-print(user_exists(db_name, tg_2))
+add_user(DB_NAME, 123456789)
+
+add_category(DB_NAME, 123456789, "Продукты")
+add_category(DB_NAME, 123456789, "Транспорт")
+add_category(DB_NAME, 123456789, "Кафе")
+add_category(DB_NAME, 123456789, "Продукты")
+
+categories = get_categories(DB_NAME, 123456789)
+
+print(categories)
